02911f8c59bd742d77809e12033021ee51be93aa
Noah Rodgers <noah@createdbynoah.com>
2023-09-04 23:01:23 -0500

    Assignment 2 - add expense year filter

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,10 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './ExpenseList.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const ExpenseList = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState(2020);
+
+  const expenseFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        onYearFilterChange={expenseFilterHandler}
+        selected={filteredYear}
+      />
       <ExpenseItem
         title={items[0].title}
         amount={items[0].amount}
