a7133ed63d07ab96beb49a52476612973f5db5c7
Noah Rodgers <noah@createdbynoah.com>
2023-09-07 11:06:05 -0500

    add additional component to render list or zero state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
 import Card from '../UI/Card';
-import './ExpenseList.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const ExpenseList = ({ items }) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,26 +16,13 @@ const ExpenseList = ({ items }) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         onYearFilterChange={expenseFilterHandler}
         selected={filteredYear}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
